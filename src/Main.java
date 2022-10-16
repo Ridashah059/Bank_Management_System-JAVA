26aa0fa9bf59f9cb35366ac19a7f04e8ecd8a7d3
Ridashah059 <115895369+Ridashah059@users.noreply.github.com>
2022-10-16 15:28:08 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
+public class BankManagementSystem extends Thread {
     public static void main(String[] args) {
+        BankManagementSystem thread = new BankManagementSystem();
+        thread.start();
+    }
+    public void run() {
         MainMenu ob = new MainMenu();
         ob.mainMenu();
     }
