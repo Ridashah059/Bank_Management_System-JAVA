fe5a6fc8806a29107987ccd079764ceb296abbfe
Amamavirk <110239718+Amamavirk@users.noreply.github.com>
2022-10-16 15:19:32 +0500

    Update WithdrawAmount.java

--- src/WithdrawAmount.java
+++ src/WithdrawAmount.java
@@ -1,18 +1,17 @@
 import javax.swing.*;
 
-public class WithdrawAmount extends AccountsData {
+class WithdrawAmount extends AccountsData {
     static int option;
     static int amount;
     public static void withdrawAmount() {
         option = Integer.parseInt(JOptionPane.showInputDialog("Enter the user Id: "));
-        amount = Integer.parseInt(JOptionPane.showInputDialog("Account Details\n\n" +
-                "Id:  " + id[option] +
-                "\nName:  " + name[option] +
-                "\nAmount:  " + salary[option] +
-                "\n\n\nEnter user Amount to Withdraw: "));
-
+        amount = Integer.parseInt(
+                JOptionPane.showInputDialog("Account Details\n\n" +
+                        "Id:  " + id[option] +
+                        "\nName:  " + name[option] +
+                        "\nAmount:  " + salary[option] +
+                        "\n\n\nEnter user Amount to Withdraw: "));
         salary[option] -= amount;
-
         JOptionPane.showMessageDialog(null,
                 "Account Details\n\n" +
                         "Id:  " + id[option] +
